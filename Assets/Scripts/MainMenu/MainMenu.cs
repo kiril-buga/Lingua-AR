52eee177cf32585cca68e638cc0102a3a2e3e315
Kiril Buga <kirilbuga@gmail.com>
2025-11-02 15:09:53 +0100

    Add VR scene and update main menu for scene loading functionality

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -3,8 +3,18 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame()
+    public void LoadARScene()
     {
         SceneManager.LoadSceneAsync(1);
+    }
+
+    public void LoadVRScene()
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
+
+    public void QuitApplication()
+    {
+        Application.Quit();
     }
 }
